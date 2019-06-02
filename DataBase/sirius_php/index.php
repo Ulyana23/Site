07903ce33b5accd8bc556cc1ad13aede10eba045
Ulyana23 <43664758+Ulyana23@users.noreply.github.com>
2019-06-02 23:18:10 +0300

    2 June (total: 1 hours) Moskalchuk
    Added a list of events.

--- DataBase/sirius_php/index.php
+++ DataBase/sirius_php/index.php
@@ -13,7 +13,7 @@
 			?>
 			<?php
 			while ($row = $result->fetch_assoc()) { ?>
-				<div><?=$row['name']?></div><hr>
+				<div><b><?=$row['name']?></b></div><hr>
 				<?php $id_region = $row['id'];
 			}
 		}?>
@@ -26,20 +26,31 @@
 			while ($row = $result->fetch_assoc()) { ?>
 				<div><div><?=$row['name']?></div>
 				<div><p><?=$row['description']?></p></div>
+				<?php $category = $row['id_category'];
+				$queryevent = "SELECT * FROM event where id_category = '$category'"; 
+				if ($resultevent = $mysqli->query($queryevent)) {
+			?>
+			<?php
+			while ($rowevent = $resultevent->fetch_assoc()) { ?>
+				<div>
+				<div><p><b>Мероприятие: </b></p></div>
+				<div><p><?=$rowevent['name']?></p></div>
+				<div><p><b>Форма: </b><?=$rowevent['form']?></p></div>
+				<div><p><b>Тип: </b><?=$rowevent['type']?></p></div>
+				<div><p><b>Начало мероприятия: </b><?=$rowevent['start_period']?></p></div>
+				<div><p><b>Конец мероприятия: </b><?=$rowevent['end_period']?></p></div>
+				<div><p><b>Запланированное число участников: </b><?=$rowevent['plan_members']?></p></div>
+				<div><p><b>Фактическое число участников: </b><?=$rowevent['fact_members']?></p></div>
+				<div><p><b>Проживание: </b><?=$rowevent['housing']?></p></div>
+				<div><p><b>Питание: </b><?=$rowevent['food']?></p></div>
 				
+				</div>
+			<?php }
+				}?>
 				</div><hr>
 				<?php
 			}
 		}?>
-		</div>
-		<div>
-		
-		</div>
-		<div>
-		
-		</div>
-		<div>
-		
 		</div>
 	</div>
 </body>
